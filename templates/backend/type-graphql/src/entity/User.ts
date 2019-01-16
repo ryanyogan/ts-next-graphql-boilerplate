ad78245bc241b064ea9bb682077137b062334646
Ryan Yogan <ryanyogan@gmail.com>
2019-01-16 12:24:14 -0800

    adding in server portion of cli

---
+++ templates/backend/type-graphql/src/entity/User.ts
@@ -0,0 +1,16 @@
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { ObjectType, Field } from "type-graphql";
+
+@Entity()
+@ObjectType()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid") id: string;
+
+  @Field()
+  @Column({ type: "text", unique: true })
+  username: string;
+
+  @Field()
+  @Column({ type: "text", unique: true })
+  email: string;
+}
